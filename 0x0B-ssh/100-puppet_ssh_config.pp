7b9b3a5661b69bd9df9c3e4fdfd0a8bf2dd825e3
Emeralda <84545872+Emeralda0144@users.noreply.github.com>
2022-10-15 13:54:25 +0100

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,17 +2,14 @@
 # to allow login w/o passord.
 
 include stdlib
-
-file_line { 'Turn off passwd auth':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
-  replace => true,
+file_line {'Declarative identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '   IdentityFile ~/.ssh/school',
+  replace => true
 }
 
-file_line { 'Delare identity file':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => '     IdentityFile ~/.ssh/school',
-  replace => true,
+file_line {'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => '     PasswordAuthentication no',
+  replace => true
 }
