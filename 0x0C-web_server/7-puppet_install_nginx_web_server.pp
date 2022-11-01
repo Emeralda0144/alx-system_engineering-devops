e0a8809fec5c81616b9a694d97c9b4591620eb6b
Emeralda0144 <kwunder014@gmail.com>
2022-11-01 12:19:16 +0100

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -8,7 +8,7 @@ file_line { 'aaaaa':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://github.com/emediongfrancis permanent;',
+  line   => 'rewrite ^/redirect_me https://github.com/Emeralda0144 permanent;',
 }
 
 file { '/var/www/html/index.html':
